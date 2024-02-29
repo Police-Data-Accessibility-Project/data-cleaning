c52538fd7ef9646246088e4da5f6ace3c2cc5e0e
maxachis <maxachis@gmail.com>
2024-02-29 18:01:18 -0500

    Update Database Manager -- Modify to account for new table structure -- Add reset_database method -- Add get_completed_entries method

--- data_processing/DatabaseManager.py
+++ data_processing/DatabaseManager.py
@@ -17,6 +17,17 @@ class DatabaseManager:
         self.connection.close()
         self.engine.dispose()
 
+    def reset_database(self):
+        """
+        Resets the database
+        :return:
+        """
+        self.connection.execute(text('''
+            DROP TABLE IF EXISTS agencies
+        '''))
+        self.connection.commit()
+        self.create_table()
+
     def create_table(self):
         """
         Creates table in the database
@@ -30,14 +41,18 @@ class DatabaseManager:
 
         """
         self.connection.execute(text('''
-        CREATE TABLE IF NOT EXISTS entries_to_validate (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            state TEXT,
-            old_url TEXT,
-            possible_correct_url TEXT,
-            reviewed BOOLEAN NOT NULL
-        )
+        CREATE TABLE IF NOT EXISTS agencies (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(255),
+            state VARCHAR(255),
+            old_url VARCHAR(255),
+            possible_correct_url VARCHAR(255),
+            airtable_uid VARCHAR(255),
+            submitted_name VARCHAR(255),
+            no_web_presence BOOLEAN,
+            rejection_reason VARCHAR(255),
+            approved BOOLEAN
+        );
         '''.strip()))
         self.connection.commit()
 
@@ -50,8 +65,28 @@ class DatabaseManager:
         """
         for entry in entries:
             self.connection.execute(text(f'''
-                INSERT INTO entries_to_validate (name, state, old_url, possible_correct_url, reviewed)
-                VALUES (:name, :state, :old_url, '', 0)
+                INSERT INTO agencies (
+                    name, 
+                    state, 
+                    old_url, 
+                    possible_correct_url, 
+                    airtable_uid, 
+                    submitted_name, 
+                    no_web_presence, 
+                    rejection_reason, 
+                    approved
+                )
+                VALUES (
+                    :name, 
+                    :state, 
+                    :old_url, 
+                    :possible_correct_url, 
+                    :airtable_uid, 
+                    :submitted_name, 
+                    :no_web_presence, 
+                    :rejection_reason, 
+                    :approved
+                )
             '''), entry)
         self.connection.commit()
 
@@ -61,7 +96,7 @@ class DatabaseManager:
         :return:
         """
         result = self.connection.execute(text('''
-            SELECT * FROM entries_to_validate WHERE possible_correct_url = ''
+            SELECT * FROM agencies WHERE possible_correct_url = ''
         '''))
         list_of_tuples = result.fetchall()
         # Convert list of tuples to list of dictionaries
@@ -86,9 +121,38 @@ class DatabaseManager:
         :return:
         """
         self.connection.execute(text('''
-            UPDATE entries_to_validate
+            UPDATE agencies
             SET possible_correct_url = :proposed_url
             WHERE id = :id
         '''), {'id': id, 'proposed_url': proposed_url})
         self.connection.commit()
 
+    def get_completed_entries(self):
+        """
+        Get entries that have been reviewed
+        :return:
+        """
+        result = self.connection.execute(text('''
+            SELECT 
+            airtable_uid,
+            submitted_name,
+            possible_correct_url,
+            approved,
+            rejection_reason
+             FROM agencies WHERE 
+            possible_correct_url != ''
+            and approved = 1
+        '''))
+        list_of_tuples = result.fetchall()
+        # Convert list of tuples to list of dictionaries
+        list_of_dicts = []
+        for item in list_of_tuples:
+            list_of_dicts.append({
+                'airtable_uid': item[0],
+                'submitted_name': item[1],
+                'homepage_url': item[2],
+                'approved': 'checked' if item[3] == 1 else 'unchecked',
+                'no_web_presence': 'unchecked',
+                'rejection_reason': item[4],
+            })
+        return list_of_dicts
