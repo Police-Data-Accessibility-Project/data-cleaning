9b0bdb061fc8e591151d4296e3470e4cb6d79527
maxachis <maxachis@gmail.com>
2024-02-29 18:04:00 -0500

    Modify to account for change in database structure

--- app/models.py
+++ app/models.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()  # Initialize the SQLAlchemy object here
 
 class DataEntry(db.Model):
-    __tablename__ = 'entries_to_validate'
+    __tablename__ = 'agencies'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    state = db.Column(db.String)
-    old_url = db.Column(db.String)
-    possible_correct_url = db.Column(db.String)
-    reviewed = db.Column(db.Boolean, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    state = db.Column(db.String(255))
+    old_url = db.Column(db.String(255))
+    possible_correct_url = db.Column(db.String(255))
+    airtable_uid = db.Column(db.String(255))
+    submitted_name = db.Column(db.String(255))
+    no_web_presence = db.Column(db.Boolean)
+    rejection_reason = db.Column(db.String(255))
+    approved = db.Column(db.Boolean)
