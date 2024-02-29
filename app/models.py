d9de93e822bc94b52e8ab7472365e72dd6f9dc9d
maxachis <maxachis@gmail.com>
2024-02-29 07:40:28 -0500

    Rename data model to reflect database

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()  # Initialize the SQLAlchemy object here
 
 class DataEntry(db.Model):
+    __tablename__ = 'entries_to_validate'
     id = db.Column(db.Integer, primary_key=True)
-    column_a = db.Column(db.String(50), nullable=False)
-    original_column_b = db.Column(db.String(50), nullable=False)
-    proposed_column_c_change = db.Column(db.String(50), nullable=False)
-    reviewed = db.Column(db.Boolean, default=False, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    state = db.Column(db.String)
+    old_url = db.Column(db.String)
+    possible_correct_url = db.Column(db.String)
+    reviewed = db.Column(db.Boolean, nullable=False)
